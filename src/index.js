13f543622f47085722634ad635428c62aad9d570
scully86 <124799557+scully86@users.noreply.github.com>
2023-07-25 06:44:05 +0530

    after ssl verification passing in index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
